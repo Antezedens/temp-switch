364b9ba7a3f2f36594dc201199962150f4d33a60
root <root@dietpi>
2017-11-07 08:05:47 +0000

    logtemp

---
+++ logtemp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ${0%%logtemp.sh}
+node logtemp.js
---
+++ setup.js
@@ -0,0 +1,28 @@
+exports.w1_prefix="/root/temp-switch/"
+
+exports.sensors = [
+{
+	gpio: "xx",
+	name: "Irgendwas"
+},
+{
+	w1_id: "1",
+	name: "Tisch"
+},
+{
+	w1_id: "2",
+	name: "Sessel"
+},	
+{
+	w1_id: "3",
+	name: "Kasten"
+},
+{
+	w1_id: "4",
+	name: "Lade"
+},
+{
+	w1_id: "5",
+	name: "Tür"
+}
+]
