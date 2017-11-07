3dcf15ddd55ed08cffa4aad62253562376b08a14
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2017-11-07 14:04:41 +0100

    parsetemp with promise

--- parsetemp.js
+++ parsetemp.js
@@ -1,33 +1,57 @@
 var fs = require('fs'),
     readline = require('readline');
 
-    
-exports.getw1temp = function(file) {    
-    var rd = readline.createInterface({
-        //input: fs.createReadStream('/sys/bus/w1/devices/28-041780d810ff/w1_slave'),
-        input: fs.createReadStream(file),
-        console: false
-    });
+var FileReader = require('filereader');
 
-    var ok = false;
-    var temp = false;
-    rd.on('line', function(line) {
-        var crc = line.search('crc=');
-        var yes = line.search('YES');
-        if (crc > 0 && yes > crc) {
-            ok = true;
-        } else {
-            var t = line.search("t=");
-            if (t && ok) {
-                temp = parseInt(line.substring(t+2)) / 1000.0;
-                console.log("ok " + temp);
-            }
-            else {
-                console.log(line);
-            }
-        }
-    });
-    return temp;
+exports.getw1tempafter = function(promise, file) {    
+    promise.then(function(v) {
+        exports.getw1temp(file);
+    })
 }
 
-console.log("temp: " + exports.getw1temp("/sys/bus/w1/devices/28-041780d810ff/w1_slave"));
+
+exports.getw1temp = function(file) {    
+
+    
+  var promise = new Promise(function(resolve, reject) {
+      var rd = readline.createInterface({
+            //input: fs.createReadStream('/sys/bus/w1/devices/28-041780d810ff/w1_slave'),
+            input: fs.createReadStream(file),
+            console: false
+        });
+
+        var ok = false;
+        var temp = false;
+        rd.on('line', function(line) {
+            var crc = line.search('crc=');
+            var yes = line.search('YES');
+            if (crc > 0 && yes > crc) {
+                ok = true;
+            } else {
+                var t = line.search("t=");
+                if (t && ok) {
+                    temp = parseInt(line.substring(t+2)) / 1000.0;
+                    console.log("ok " + temp);
+                }
+                else {
+                    console.log(line);
+                }
+            }
+        });
+        rd.on('close', function(line) {
+            resolve([temp]);
+        });
+    });
+  
+    return promise;
+}
+
+//var p = exports.getw1temp("/sys/bus/w1/devices/28-041780d810ff/w1_slave")
+var t1 = exports.getw1temp("/sys/bus/w1/devices/28-041780d810ff/w1_slave")
+t1.then(function(v) {
+    console.log("temp: " + v[0]);
+})
+
+/*Promise.all([t0, t1]).then(function(v) {
+    console.log("temp: " + v[0] + " " + v[1]);    
+});*/
