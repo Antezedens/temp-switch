f2576aef3a1413be6e3ee2f52a01237d62968565
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2017-11-07 14:24:32 +0100

    parsetemp...

--- parsetemp.js
+++ parsetemp.js
@@ -9,45 +9,54 @@ exports.getw1tempafter = function(promise, file) {
     })
 }
 
+function getw1tempinternal(files, resolve, result) {
+    resolve(result);
+}
 
-exports.getw1temp = function(file) {    
+function readw1(file, then) {
+    var rd = readline.createInterface({
+        //input: fs.createReadStream('/sys/bus/w1/devices/28-041780d810ff/w1_slave'),
+        input: fs.createReadStream(file),
+        console: false
+    });
+
+    var ok = false;
+    var temp = false;
+    rd.on('line', function(line) {
+        var crc = line.search('crc=');
+        var yes = line.search('YES');
+        if (crc > 0 && yes > crc) {
+            ok = true;
+        } else {
+            var t = line.search("t=");
+            if (t && ok) {
+                temp = parseInt(line.substring(t+2)) / 1000.0;
+                console.log("ok " + temp);
+            }
+            else {
+                console.log(line);
+            }
+        }
+    });
+    rd.on('close', function(line) {
+        then(temp);
+    });    
+}
+
+exports.getw1temps = function(files) {    
 
     
   var promise = new Promise(function(resolve, reject) {
-      var rd = readline.createInterface({
-            //input: fs.createReadStream('/sys/bus/w1/devices/28-041780d810ff/w1_slave'),
-            input: fs.createReadStream(file),
-            console: false
-        });
-
-        var ok = false;
-        var temp = false;
-        rd.on('line', function(line) {
-            var crc = line.search('crc=');
-            var yes = line.search('YES');
-            if (crc > 0 && yes > crc) {
-                ok = true;
-            } else {
-                var t = line.search("t=");
-                if (t && ok) {
-                    temp = parseInt(line.substring(t+2)) / 1000.0;
-                    console.log("ok " + temp);
-                }
-                else {
-                    console.log(line);
-                }
-            }
-        });
-        rd.on('close', function(line) {
-            resolve([temp]);
-        });
+      readw1(files[0], function(temp) {
+          getw1tempinternal(files, resolve, [temp]);
+      });
     });
   
     return promise;
 }
 
 //var p = exports.getw1temp("/sys/bus/w1/devices/28-041780d810ff/w1_slave")
-var t1 = exports.getw1temp("/sys/bus/w1/devices/28-041780d810ff/w1_slave")
+var t1 = exports.getw1temps(["/sys/bus/w1/devices/28-041780d810ff/w1_slave"])
 t1.then(function(v) {
     console.log("temp: " + v[0]);
 })
