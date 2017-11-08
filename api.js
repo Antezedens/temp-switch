c3f8268eed5b251e66d2957cba9f01a3c17fd896
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2017-11-08 15:12:05 +0100

    relais from json file

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 var util = require('util');
 var exec = require('child_process').exec;
 var setup = require('./setup');
+var jf = require('jsonfile')
 const sqlite3 = require('sqlite3').verbose();
 let errfct = function($err) {
         if ($err) {
@@ -10,19 +11,16 @@ let errfct = function($err) {
 
 let db = new sqlite3.Database('temp.sql', errfct);
 
- 
+
 // GET
 exports.relais = function (req, res) {
   console.log('Getting switches.');
-  data = [];
-  
-  data.push({name: "Fan", state: "on", timeout: "00:00:00"});
-  data.push({name: "FilterOn", state: "off", timeout: "00:10:00"});
-  data.push({name: "FilterOff", state: "off", timeout: "00:10:00"});
+  jf.readFile('./relais.json', function(err, obj) {
+    res.status(200).json(obj);
+  });
 
-  res.status(200).json(data);
 };
- 
+
 // PUT
 exports.setRelais = function (req, res) {
   var name = req.body.name;
@@ -35,7 +33,6 @@ exports.sensors = function (req, res) {
 	var data = {};
 	var temps;
 	db.get("SELECT * FROM temp ORDER BY date DESC LIMIT 1", function(err, row) {
-		console.log("last row " + row);
 		data.humidity = row.h0;
 		temps = [row.t0, row.t1, row.t2, row.t3, row.t4, row.t5];
 
@@ -49,7 +46,7 @@ exports.sensors = function (req, res) {
 		res.json(data);
 	});
 }
- 
+
 function switchStatus(script, command, status){
     var execString = script + " " + command + " " + status;
     console.log("Executing: " + execString);
