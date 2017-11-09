f2c8851bb232254842dda44bfb347173b7ac58ce
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2017-11-09 18:06:17 +0100

    gzip compressed temp data

--- api.js
+++ api.js
@@ -2,7 +2,9 @@ var util = require('util');
 var exec = require('child_process').exec;
 var setup = require('./setup');
 var jf = require('jsonfile');
+const fs = require('fs');
 var checkrelaisstate = require('./checkrelaisstate.js');
+const zlib = require('zlib');
 const sqlite3 = require('sqlite3').verbose();
 const relaisFile = './relais.json';
 let errfct = function($err) {
@@ -61,6 +63,42 @@ exports.sensors = function (req, res) {
 		}
 		res.json(data);
 	});
+}
+
+exports.temperatures = function (req, res) {
+  /*fs.writeFileSync("/tmp/test.txt", "[[2, 100], [3,120], [4, 110]]");
+  const gzip = zlib.createGzip();
+  const inp = fs.createReadStream('/tmp/test.txt');
+  /*const out = fs.createWriteStream('/tmp/test.gz');
+  inp.pipe(gzip).pipe(out);*/
+  let buffer = zlib.gzipSync("[[2, 100], [3,120], [4, 110]]");
+  //console.log()
+
+  res.set('Content-Encoding', 'gzip');
+  //res.set('Content-type', 'application/x-gzip');
+  //let buffer = fs.readFileSync('/tmp/test.gz', 'binary')
+  res.write(buffer, 'binary');
+
+  res.status(200).end(null, 'binary');
+
+  
+	/*var data = {};
+	var temps;
+  var zip = new require('node-zip');
+	db.each("SELECT date, t0, t1, t2, t3, t4, t5 FROM temp ORDER BY date", function(err, row) {
+		temps = [row.t0, row.t1, row.t2, row.t3, row.t4, row.t5];
+
+		data.temperatures = [];
+		for (let i=0; i<setup.sensors.length; ++i) {
+			var temp = {};
+			temp.name = setup.sensors[i].name;
+			temp.value = temps[i];
+			data.temperatures.push(temp)
+		}
+		res.json(data);
+	}, function(err) {
+
+  });*/
 }
 
 function switchStatus(script, command, status){
