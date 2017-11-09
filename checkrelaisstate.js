5e9c76da6ef1fb7a390fea916557a12ec4a92164
Bernhard <bernhard.fux@gmail.com>
2017-11-09 08:08:02 +0000

    fix relais state

--- checkrelaisstate.js
+++ checkrelaisstate.js
@@ -2,18 +2,25 @@ var fs = require('fs');
 const gpioBasePath = "/sys/class/gpio/"
 
 function gpioState(pin, state) {
-  let value = gpioBasePath + "gpio" + pin + "/value";
-  let direction = gpioBasePath + "gpio" + pin + "/direction";
-  if (!fs.existsSync(value)) {
+  let valuePath = gpioBasePath + "gpio" + pin + "/value";
+  let directionPath = gpioBasePath + "gpio" + pin + "/direction";
+  if (!fs.existsSync(valuePath)) {
     fs.writeFileSync(gpioBasePath + 'export', "" + pin);
   }
-  console.log(fs.readFileSync(value));
-  console.log(fs.readFileSync(direction));
+  if (fs.readFileSync(directionPath).toString() != "out\n") {
+    console.log("updated direction of " + pin);
+    fs.writeFileSync(directionPath, "out");
+  }
+  let value = state ? "0\n" : "1\n";
+
+  if (fs.readFileSync(valuePath).toString() != value) {
+    console.log("updated value of " + pin);
+    fs.writeFileSync(valuePath, value);
+  }
 }
 
 exports.update = function(relais) {
   for (let i=0; i<relais.length; ++i) {
-    console.log("checking relais: " + relais[i].name);
     gpioState(relais[i].gpio, relais[i].on);
   }
 }
