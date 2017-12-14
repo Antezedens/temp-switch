e3cacf8a3eb59a21c727d3e880a5f93639f63acf
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2017-12-14 13:15:32 +0100

    incremental data download

--- api.js
+++ api.js
@@ -6,9 +6,9 @@ var checkrelaisstate = require('./checkrelaisstate.js');
 const zlib = require('zlib');
 const sqlite3 = require('sqlite3').verbose();
 let errfct = function($err) {
-        if ($err) {
-                return console.error($err.message);
-        }
+    if ($err) {
+        return console.error($err.message);
+    }
 }
 
 let db = new sqlite3.Database('temp.sql', errfct);
@@ -16,112 +16,171 @@ let db2 = new sqlite3.Database('relais.sql', errfct);
 
 
 // GET
-exports.relais = function (req, res) {
-  console.log('Getting switches.');
-  res.status(200).json(checkrelaisstate.readRelais());
+exports.relais = function(req, res) {
+    console.log('Getting switches.');
+    res.status(200).json(checkrelaisstate.readRelais());
 };
 
 // PUT
-exports.setRelais = function (req, res) {
-  var body = req.body;
-  var name = body.name;
-  console.log("Name: " + name + " : " + body);
+exports.setRelais = function(req, res) {
+    var body = req.body;
+    var name = body.name;
+    console.log("Name: " + name + " : " + body);
 
-  relais = checkrelaisstate.readRelais();
-  for (let i=0; i<relais.length; ++i) {
-    if (relais[i].name == name) {
-      if ('state' in body) {
-        relais[i].on = body.state;
-	if (body.state && 'excludes' in relais[i]) {
-	  let excludes = relais[i].excludes;
-	  for (let j=0; j<relais.length; ++j) {
-	    if (relais[j].name == excludes || relais[j].gpio == excludes) {
-              relais[j].on = false;
-              relais[j].switching = "";
+    relais = checkrelaisstate.readRelais();
+    for (let i = 0; i < relais.length; ++i) {
+        if (relais[i].name == name) {
+            if ('state' in body) {
+                relais[i].on = body.state;
+                if (body.state && 'excludes' in relais[i]) {
+                    let excludes = relais[i].excludes;
+                    for (let j = 0; j < relais.length; ++j) {
+                        if (relais[j].name == excludes || relais[j].gpio == excludes) {
+                            relais[j].on = false;
+                            relais[j].switching = "";
+                        }
+                    }
+                }
             }
-	  }
-	}
-      }
-      if ('switching' in body) {
-        relais[i].switching = body.switching;
-      }
+            if ('switching' in body) {
+                relais[i].switching = body.switching;
+            }
+        }
     }
-  }
-  checkrelaisstate.writeRelais(relais);
-  res.status(200).send();
+    checkrelaisstate.writeRelais(relais);
+    res.status(200).send();
 };
 
-exports.sensors = function (req, res) {
-	var data = {};
-	var temps;
-	db.get("SELECT * FROM temp ORDER BY date DESC LIMIT 1", function(err, row) {
-		data.humidity = row.h0;
-		temps = [row.t0, row.t1, row.t2, row.t3, row.t4, row.t5, row.cput];
+exports.sensors = function(req, res) {
+    var data = {};
+    var temps;
+    db.get("SELECT * FROM temp ORDER BY date DESC LIMIT 1", function(err, row) {
+        data.humidity = row.h0;
+        temps = [row.t0, row.t1, row.t2, row.t3, row.t4, row.t5, row.cput];
 
-		data.temperatures = [];
-		for (let i=0; i<setup.sensors.length; ++i) {
-			var temp = {};
-			temp.name = setup.sensors[i].name;
-			temp.value = temps[i];
-			data.temperatures.push(temp)
-		}
-		res.json(data);
-	});
+        data.temperatures = [];
+        for (let i = 0; i < setup.sensors.length; ++i) {
+            var temp = {};
+            temp.name = setup.sensors[i].name;
+            temp.value = temps[i];
+            data.temperatures.push(temp)
+        }
+        res.json(data);
+    });
 }
 
 function f(s) {
-  return " round(t" + s + ", 2) as t" + s + ",";
+    return " round(t" + s + ", 2) as t" + s + ",";
 }
 
-exports.temperatures = function (req, res) {
-  /*fs.writeFileSync("/tmp/test.txt", "[[2, 100], [3,120], [4, 110]]");
-  const gzip = zlib.createGzip();
-  const inp = fs.createReadStream('/tmp/test.txt');
-  /*const out = fs.createWriteStream('/tmp/test.gz');
-  inp.pipe(gzip).pipe(out);*/
-  //console.log()
+exports.temperatures = function(req, res) {
+    var fromts = req.query.ts;
+    /*fs.writeFileSync("/tmp/test.txt", "[[2, 100], [3,120], [4, 110]]");
+    const gzip = zlib.createGzip();
+    const inp = fs.createReadStream('/tmp/test.txt');
+    /*const out = fs.createWriteStream('/tmp/test.gz');
+    inp.pipe(gzip).pipe(out);*/
+    //console.log()
 
-  res.set('Content-Encoding', 'gzip');
-  //res.set('Content-type', 'application/x-gzip');
-  //let buffer = fs.readFileSync('/tmp/test.gz', 'binary')
-		var names = [];
-		for (let i=0; i<setup.sensors.length; ++i) {
-			names.push(setup.sensors[i].name);
-		}
+    res.set('Content-Encoding', 'gzip');
+    //res.set('Content-type', 'application/x-gzip');
+    //let buffer = fs.readFileSync('/tmp/test.gz', 'binary')
+    var names = [];
+    var data = [
+        []
+    ];
 
-	var data = [];
-	db.each("SELECT strftime('%s', date) as ts," + f(0) + f(1) + f(2) + f(3) + f(4) + f(5) + " cput FROM temp ORDER BY date", function(err, row) {
-    data.push([row.ts, row.t0, row.t1, row.t2, row.t3, row.t4, row.t5, row.cput]);
-	}, function(err) {
-    let buffer = zlib.gzipSync(JSON.stringify({names: names, data: data}));
-    res.set('Content-Length', buffer.length);
-    res.write(buffer, 'binary');
+    for (let i = 0; i < setup.sensors.length; ++i) {
+        names.push(setup.sensors[i].name);
+        data.push([]);
+    }
 
-    res.status(200).end(null, 'binary');
+    var lastts = 0;
+    var lastvalues = [0, 0, 0, 0, 0, 0];
+    var lasttss = [0, 0, 0, 0, 0, 0];
+    var transition = [];
 
-  });
+    function addpoint(ts, i, value, lasttss) {
+        if (ts < fromts[i]) {
+            return;
+        }
+
+        if (lastvalues[i] != value) {
+            var doit = true;
+            if (Math.abs(lastvalues[i] - value) < 0.15) {
+                if (transition[i] === null) {
+                    transition[i] = ts;
+                    doit = false;
+                } else {
+                    if (ts - transition[i] > 17 * 60) { // some minutes
+                        data[i].push([transition[i], lastvalues[i]]);
+                        lasttss[i] = lastts;
+                        doit = true;
+                    } else {
+                        doit = false;
+                    }
+                }
+            }
+            if (doit) {
+                if (lastts != lasttss[i] && lasttss[i] != 0) {
+                    data[i].push([lastts, lastvalues[i]]);
+                }
+
+                data[i].push([ts, value]);
+                lastvalues[i] = value;
+                lasttss = ts;
+                transition[i] = null;
+            }
+        }
+    }
+
+    db.each("SELECT strftime('%s', date) as ts," + f(0) + f(1) + f(2) + f(3) + f(4) + f(5) + " cput FROM temp ORDER BY date", function(err, row) {
+        let ts = row.ts;
+        addpoint(ts, 0, row.t0, lasttss);
+        addpoint(ts, 1, row.t1, lasttss);
+        addpoint(ts, 2, row.t2, lasttss);
+        addpoint(ts, 3, row.t3, lasttss);
+        addpoint(ts, 4, row.t4, lasttss);
+        addpoint(ts, 5, row.t5, lasttss);
+        addpoint(ts, 6, row.cput, lasttss);
+        lastts = ts;
+        //data.push([row.ts, row.t0, row.t1, row.t2, row.t3, row.t4, row.t5, row.cput]);
+    }, function(err) {
+        let buffer = zlib.gzipSync(JSON.stringify({
+            names: names,
+            data: data,
+            lastts: lastts
+        }));
+        res.set('Content-Length', buffer.length);
+        res.write(buffer, 'binary');
+
+        res.status(200).end(null, 'binary');
+
+    });
 }
 
-exports.relaisHistory = function (req, res) {
-	res.set('Content-Encoding', 'gzip');
-	var data = {};
+exports.relaisHistory = function(req, res) {
+    res.set('Content-Encoding', 'gzip');
+    var data = {};
 
-  	relais = checkrelaisstate.readRelais();
-  	for (let i=0; i<relais.length; ++i) {
-	    	data[relais[i].gpio] = [];
-  	}
-	db2.each("SELECT  strftime('%s', date) as ts, gpio, state FROM relais ORDER BY date", function(err, row) {
-		data[row.gpio].push([row.ts, row.state]);
+    relais = checkrelaisstate.readRelais();
+    for (let i = 0; i < relais.length; ++i) {
+        data[relais[i].gpio] = [];
+    }
+    db2.each("SELECT  strftime('%s', date) as ts, gpio, state FROM relais ORDER BY date", function(err, row) {
+        data[row.gpio].push([row.ts, row.state]);
 
-	}, function(err) {
-		let buffer = zlib.gzipSync(JSON.stringify({data: data}));
-		res.write(buffer, 'binary');
-		res.status(200).end(null, 'binary');
-	});
+    }, function(err) {
+        let buffer = zlib.gzipSync(JSON.stringify({
+            data: data
+        }));
+        res.write(buffer, 'binary');
+        res.status(200).end(null, 'binary');
+    });
 
 }
 
-function switchStatus(script, command, status){
+function switchStatus(script, command, status) {
     var execString = script + " " + command + " " + status;
     console.log("Executing: " + execString);
     exec(execString, puts);
---
+++ html/graph.js
@@ -0,0 +1,271 @@
+var series = [];
+var myChart;
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function($scope, $http) {
+    function updateSeries() {
+        var seriesNew = [];
+        if ($scope.selected_temp != null) {
+            for (let i = 0; i < series.length; ++i) {
+                if (series[i].name == $scope.selected_temp) {
+                    seriesNew.push({
+                        name: series[i].name,
+                        data: series[i].data,
+                        type: 'area'
+                    });
+                }
+            }
+        } else {
+            for (let i = 0; i < 6; ++i) {
+                seriesNew.push(series[i]);
+            }
+        }
+        for (x in $scope.relais) {
+            if ($scope.relais[x].shown) {
+                seriesNew.push({
+                    name: $scope.relais[x].name,
+                    yAxis: 1,
+                    data: $scope.relaisHistory[$scope.relais[x].gpio]
+                });
+            }
+        }
+        myChart.update({
+            series: seriesNew
+        }, true, true);
+    }
+
+    $scope.updateRelais = function() {
+        $http.get("/relais")
+            .then(function(response) {
+                $scope.resp = response;
+                $scope.relais = response.data;
+                for (i = 0; i < $scope.relais.length; ++i) {
+                    $scope.relais[i].checked = $scope.relais[i].on
+                    if ($scope.relais[i].switching != "") {
+                        let d = new Date($scope.relais[i].switching);
+                        //let d = $scope.relais[i].switching;
+                        $scope.relais[i].switchingTime = d.format('dd.mm. HH:MM');
+                    }
+                }
+            }, function(err) {
+                $scope.resp = err;
+            });
+    };
+
+    $scope.updateRelais();
+
+    $scope.selected_temp = null;
+    $scope.setRelais = function(name, checked) {
+        $http.post("/relais", {
+            "name": name,
+            "state": checked,
+            "switching": ""
+        }).then(function(response) {
+            $scope.updateRelais();
+        });
+    };
+    $scope.setRelais24 = function(name) {
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        $http.post("/relais", {
+            "name": name,
+            "state": true,
+            "switching": tomorrow
+        }).then(function(response) {
+            $scope.updateRelais();
+        });
+    }
+    $scope.toggle = function(name) {
+        if ($scope.selected_temp == name) {
+            $scope.selected_temp = null;
+        } else {
+            $scope.selected_temp = name;
+        }
+        updateSeries();
+    }
+    $scope.toggleRelais = function(x) {
+        x.shown = !x.shown;
+        updateSeries();
+    }
+    //$http.get("http://private-699939-technikschacht.apiary-mock.com/relais")
+
+    $http.get("/sensors")
+        .then(function(response) {
+            $scope.sensors = response.data;
+        });
+
+    //indexedDB.deleteDatabase('sensors');
+    var request = indexedDB.open('sensors', 1);
+    request.onupgradeneeded = function() {
+        var db = this.result;
+        for (let i = 0; i < 10; ++i) {
+            if (!db.objectStoreNames.contains('data' + i)) {
+                db.createObjectStore('data' + i, {
+                    keyPath: "date"
+                })
+            }
+        }
+    };
+    request.onsuccess = function() {
+        var db = this.result;
+        var fetchts = [];
+        for (let i = 0; i < 10; ++i) {
+            fetchts.push(new Promise(function(fulfill, reject) {
+                var readtrans = db.transaction(['data' + i], 'readonly');
+                var readStore = readtrans.objectStore('data' + i);
+                readStore.openCursor(null, 'prev').onsuccess = function(event) {
+                    var cur = event.target.result;
+                    if (cur) {
+                        console.log(cur.key);
+                        fulfill(cur.key / 1000);
+                    } else {
+                        fulfill(0);
+                    }
+                };
+            }));
+        }
+        Promise.all(fetchts).then(latesttimestamps => {
+            $http.get("/temperatures", {
+                    params: {
+                        ts: latesttimestamps
+                    }
+                })
+                .then(function(response) {
+                    $scope.datasize = response.headers('Content-Length');
+                    let t = response.data.data;
+                    let names = response.data.names;
+                    var promises = [];
+                    var verylastts = response.data.lastts * 1000;
+
+                    for (let i = 0; i < names.length; ++i) {
+                        promises.push(new Promise(function(fulfill, reject) {
+                            var serdata = [];
+                            var trans = db.transaction(['data' + i], 'readwrite');
+                            var store = trans.objectStore('data' + i);
+                            for (let j = 0; j < t[i].length; ++j) {
+                                store.put({
+                                    date: t[i][j][0] * 1000,
+                                    value: t[i][j][1]
+                                });
+                            }
+                            var read = store.openCursor();
+                            read.onsuccess = function(event) {
+                                var cursor = event.target.result;
+                                if (cursor) {
+                                    let entry = [cursor.value.date, cursor.value.value];
+                                    serdata.push(entry);
+                                    cursor.continue();
+                                } else {
+                                    if (serdata.length > 0) {
+                                        var lastentry = serdata[serdata.length - 1];
+                                        serdata.push([verylastts, lastentry[1]]);
+                                    }
+                                    fulfill(serdata);
+                                }
+                            }
+                        }));
+                    }
+                    $http.get("/relaisHistory")
+                        .then(function(response) {
+                            Promise.all(promises).then(values => {
+                                for (let i = 0; i < values.length; ++i) {
+                                    series.push({
+                                        name: names[i],
+                                        data: values[i]
+                                    });
+                                }
+                                for (gpio in response.data.data) {
+                                    let gp = response.data.data[gpio];
+                                    let gpNew = [];
+                                    console.log(gp);
+                                    for (let i = 0; i < gp.length; ++i) {
+                                        if (i > 0 && gp[i][1] != gp[i - 1][1]) {
+                                            gpNew.push([(gp[i][0] - 1) * 1000, gp[i - 1][1]]);
+                                        }
+                                        gpNew.push([gp[i][0] * 1000, gp[i][1]]);
+                                    }
+                                    gpNew.push([verylastts, gp[gp.length - 1][1]]);
+                                    response.data.data[gpio] = gpNew;
+                                }
+                                $scope.relaisHistory = response.data.data;
+                                console.log($scope.relaisHistory);
+                                updateSeries();
+                            })
+                        });
+
+                }, function(err) {
+                    console.log(err);
+                });
+        });
+    };
+
+
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+    myChart = Highcharts.chart('container', {
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: 'Temperaturverlauf'
+        },
+        subtitle: {
+            //text: document.ontouchstart === undefined ?
+            //        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: [{
+            labels: {
+                format: '{value}°C'
+            },
+            title: {
+                text: 'Temperature'
+            },
+        }, {
+            ceiling: 1,
+            opposite: true,
+            title: {
+                text: 'State'
+            }
+        }],
+        legend: {
+            enabled: true
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+
+        },
+
+        series: series.slice(0, 6)
+    });
+
+    //$http.get("http://private-699939-technikschacht.apiary-mock.com/sensors")
+});
