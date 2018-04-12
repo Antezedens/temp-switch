c806c56a6313c0477a0dbeb8b17a89e558e9abd7
Bernhard <bernhard.fux@gmail.com>
2018-04-12 11:37:04 +0100

    improved dht22 thing

--- dht22/dht22.c
+++ dht22/dht22.c
@@ -12,12 +12,70 @@
 #include <stdint.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
 
 #include "locking.h"
 
 #define MAXTIMINGS 85
-static int DHTPIN = 7;
+static int DHTPIN = 10;
 static int dht22_dat[5] = {0,0,0,0,0};
+
+void delayMicroseconds(int us) {
+	struct timespec ts;
+	clock_gettime(CLOCK_REALTIME, &ts);
+	ts.tv_nsec += us * 1000;
+	if (ts.tv_nsec > 1000 * 1000 * 1000) {
+		ts.tv_sec ++;
+		ts.tv_nsec -= 1000 * 1000 * 1000;
+	}
+	struct timespec ts2;
+	do {
+		clock_gettime(CLOCK_REALTIME, &ts2);
+		if (ts2.tv_sec == ts.tv_sec && ts2.tv_nsec > ts.tv_nsec) {
+			break;
+		}
+		if (ts2.tv_sec > ts.tv_sec) {
+			break;
+		}
+	} while (1);
+	//printf("%d.%09d -> %d.%09d\n", ts.tv_sec, ts.tv_nsec, ts2.tv_sec, ts2.tv_nsec);
+}
+void delay(int ms) {
+	delayMicroseconds(ms * 1000);
+}
+
+
+void pinMode(int pin, int inout) {
+	int file = open("/sys/class/gpio/gpio10/direction", O_WRONLY);
+	if (inout == OUTPUT) {
+		write(file, "out\n", 4);
+	} else {
+		write(file, "in\n", 3);
+	}
+	close(file);
+}
+
+void digitalWrite(int pin, int value) {
+	int file = open("/sys/class/gpio/gpio10/value", O_WRONLY);
+	if (value == HIGH) {
+		write(file, "1\n", 2);
+	} else {
+		write(file, "0\n", 2);
+	}
+	close(file);
+}
+int digitalRead(int pin) {
+	int result;
+	int file = open("/sys/class/gpio/gpio10/value", O_RDONLY);
+	char v = 'x';
+	read(file, &v, 1);
+	close(file);
+	result = v == '1';
+	//printf("%c>%d\n", v, result);
+	return result;
+}
+
 
 static uint8_t sizecvt(const int read)
 {
@@ -93,13 +151,26 @@ static int read_dht22_dat()
   }
   else
   {
-    printf("Data not good, skip\n");
+    printf("Data not good, skip %02x-%02x-%02x-%02x = %02x\n", dht22_dat[0], dht22_dat[1], dht22_dat[2], dht22_dat[3], dht22_dat[4]);
     return 0;
   }
 }
 
+void testdelay(void) {
+	struct timespec ts;
+	struct timespec ts2;
+	clock_gettime(CLOCK_REALTIME, &ts);
+	delayMicroseconds(15);
+	//delay(10);
+	clock_gettime(CLOCK_REALTIME, &ts2);
+	printf("%ld -> %ld = %ld\n", ts.tv_nsec, ts2.tv_nsec, ts2.tv_nsec - ts.tv_nsec);
+}
+
+
 int main (int argc, char *argv[])
 {
+testdelay();
+
   int lockfd;
   int tries = 100;
 
@@ -117,13 +188,18 @@ int main (int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
-  printf ("Raspberry Pi wiringPi DHT22 reader\nwww.lolware.net\n") ;
+  printf ("Raspberry Pi wiringPi DHT22 reader\nwww.lolware.net\nPIN: %d\n", DHTPIN) ;
 
   lockfd = open_lockfile(LOCKFILE);
 
-  if (wiringPiSetup () == -1)
+  if (wiringPiSetupGpio () == -1)
     exit(EXIT_FAILURE) ;
 	
+
+  //pinMode(DHTPIN, OUTPUT);
+  //printf("mode out exit\n");
+  //return -1;
+
   if (setuid(getuid()) < 0)
   {
     perror("Dropping privileges failed\n");
