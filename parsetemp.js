b94e7568fc5da75281e644b44dca537252a4f81e
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2018-04-18 12:14:54 +0200

    improve logtemp / parsetemp

--- parsetemp.js
+++ parsetemp.js
@@ -3,21 +3,17 @@ var fs = require('fs'),
 
 var execSync = require('child_process').execSync;
 
-exports.getw1tempafter = function(promise, file) {
-    promise.then(function(v) {
-        exports.getw1temp(file);
-    })
-}
-
 exports.getDHT22 = function(pin) {
     for (var i = 0; i < 50; ++i) {
         try {
             var output = execSync("dht22/dht " + pin).toString('utf8');
             let result = output.split(" ");
-            return {
+            let out = {
                 humidity: parseFloat(result[0].split("=")[1]),
                 temp: parseFloat(result[1].split("=")[1])
             };
+            console.log('h/t:' + out['humidity'] + "/" + out['temp']);
+            return out;
         } catch (e) {
             console.log("Failed once...");
         }
@@ -32,47 +28,34 @@ function getw1tempinternal(files, resolve, result) {
     resolve(result);
 }
 
-function readw1(files, resolve, results) {
-    var rd = readline.createInterface({
-        //input: fs.createReadStream('/sys/bus/w1/devices/28-041780d810ff/w1_slave'),
-        input: fs.createReadStream(files[0]),
-        console: false
-    });
+function readw1(file) {
+  try {
+    var line = fs.readFileSync(file).toString();
+  } catch(e) {
+    return "null";
+  }
 
-    var ok = false;
-    var temp = false;
-    rd.on('line', function(line) {
-        var crc = line.search('crc=');
-        var yes = line.search('YES');
-        if (crc > 0 && yes > crc) {
-            ok = true;
-        } else {
-            var t = line.search("t=");
-            if (t && ok) {
-                temp = parseInt(line.substring(t + 2)) / 1000.0;
-                console.log("ok " + temp);
-            } else {
-                console.log(line);
-            }
-        }
-    });
-    rd.on('close', function(line) {
-        results.push(temp);
-        if (files.length <= 1) {
-            resolve(results);
-        } else {
-            files.shift();
-            readw1(files, resolve, results);
-        }
-    });
+  var crc = line.search('crc=');
+  var yes = line.search('YES');
+  if (crc <= 0 || yes < crc) {
+    return "null";
+  }
+  var t = line.search("t=");
+  if (t) {
+      temp = parseInt(line.substring(t + 2)) / 1000.0;
+      console.log("ok " + temp);
+      return temp;
+  } else {
+    console.log(file);
+    return "null";
+  }
 }
 
 exports.getw1temps = function(files) {
-
-
-    var promise = new Promise(function(resolve, reject) {
-        readw1(files, resolve, []);
-    });
-
-    return promise;
+  console.log(files);
+  var result = [];
+  for (var i=0; i<files.length; ++i) {
+    result.push(readw1(files[i]));
+  }
+  return result;
 }
