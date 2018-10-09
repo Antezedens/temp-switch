009aa2339880692ce84a3f8ecd1a24bd13ee17a0
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2018-10-09 18:13:41 +0200

    make logtemp2 send sensors values only if they change

--- logtemp2.js
+++ logtemp2.js
@@ -23,43 +23,108 @@ try {
 	cput = "null";
 }
 
-function addtemp(data, ts, value, id) {
+function datapush(data, ts, value) {
+	data.push([dateformat(ts, "yyyy-mm-dd HH:MM"), id, value]);
+}
+
+function addpoint(ts, td, value, delta, data, lastts, id) {
+		if (td.lastvalues != value 
+			&& value > -50 // ignore impossible values!
+		) { 
+				var doit = true;
+
+				if (Math.abs(td.lastvalues - value) < delta) {
+						if (td.transition === null) {
+								td.transition = ts;
+								doit = false;
+						} else {
+								if (ts - td.transition > 17 * 60 * 1000) { // 17 minutes
+									  datapush(data, id, td.transition, td.lastvalues);
+										td.lasttss = lastts;
+										doit = true;
+								} else {
+										doit = false;
+								}
+						}
+				}
+				if (doit) {
+						if (lastts != td.lasttss && td.lasttss != 0) {
+							datapush(data, id, lastts, td.lastvalues);
+						}
+
+						datapush(data, id, ts,value);
+						td.lastvalues = value;
+						td.lasttss = ts;
+						td.transition = null;
+				}
+		} else {
+				td.transition = null;
+		}
+}
+
+
+function addtemp(data, ts, value, id, delta, transdata) {
 	if (value != "null" && value != null) {
-		data.push([ts,id,value]);
+		if (!(id in transdata.data)) {
+			transdata.data[id] = {
+				lasttss: 0,
+				lastvalues: 0,
+				transition: null
+			};
+		}
+	  
+		addpoint(ts, transdata.data[id], value, delta, data, transdata.lastts, id);
 	}
 }
 
 const laterfile = '/tmp/sensors.later';
+const transfile = '/tmp/transition.json';
 parsetemp.getinternet((temp, humid) => {
-	var ts = dateformat(Date.now(), "yyyy-mm-dd HH:MM");
+
+	var transdata = {
+		lastts: 0,
+		data: {}
+	};
+	try {
+		transdata = JSON.parse(fs.readFileSync(transfile));
+	} catch (e) {}
+
+	var ts = Date.now();
 	var postdata = [];
 	try {
 		postdata = JSON.parse(fs.readFileSync(laterfile));
 		fs.unlinkSync(laterfile);
 	} catch(e) {		
 	}
-	addtemp(postdata, ts, humid, 100);
-	addtemp(postdata, ts, dht22['humidity'], 101);
-	addtemp(postdata, ts, temp, 0);
-	addtemp(postdata, ts, dht22['temp'],1);
-	addtemp(postdata, ts, cput, 2);
 
-	addtemp(postdata, ts, v[0], 3);
-	addtemp(postdata, ts, v[1], 4);
-	addtemp(postdata, ts, v[2], 5);
-	addtemp(postdata, ts, v[3], 6);
-	addtemp(postdata, ts, v[4], 7);
+	addtemp(postdata, ts, humid, 100, 1.5, transdata);
+	addtemp(postdata, ts, dht22['humidity'], 101, 0.15, transdata);
+	addtemp(postdata, ts, temp, 0, 0.15, transdata);
+	addtemp(postdata, ts, dht22['temp'],1, 0.15, transdata);
+	addtemp(postdata, ts, cput, 2, 1.5, transdata);
+
+	addtemp(postdata, ts, v[0], 3, 0.15, transdata);
+	addtemp(postdata, ts, v[1], 4, 0.15, transdata);
+	addtemp(postdata, ts, v[2], 5, 0.15, transdata);
+	addtemp(postdata, ts, v[3], 6, 0.15, transdata);
+	addtemp(postdata, ts, v[4], 7, 0.15, transdata);
 	
+	transdata.lastts = ts;
+	fs.writeFileSync(transfile, JSON.stringify(transdata));
+
 	console.log(postdata);
-	//request({url: 'http://fuchsbau.cu.ma/sensor.php', method: "POST", json: false, body: "data=" + postdata}, function (error, response, body) {
-	request.post('http://fuchsbau.cu.ma/sensor.php', { json: postdata}, function (error, response, body) {
-		if (error) {
-			console.log("error: " + error);
-			fs.writeFileSync(laterfile, JSON.stringify(postdata));
-		} else {
-		  console.log(response);
-	  }
-	});
+	
+	if (postdata.length > 0) {
+		//request({url: 'http://fuchsbau.cu.ma/sensor.php', method: "POST", json: false, body: "data=" + postdata}, function (error, response, body) {
+		request.post('http://fuchsbau.cu.ma/sensor.php', { json: postdata}, function (error, response, body) {
+			if (error) {
+				console.log("error: " + error);
+				fs.writeFileSync(laterfile, JSON.stringify(postdata));
+			} else {
+			  console.log(response);
+		  }
+		});
+	}
 });
 
 require('./checkrelaisstate').check();
