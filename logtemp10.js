756d6eaafe48f4d0ecb143a945c28ceba6ea6bae
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2019-06-03 16:54:44 +0200

    generalize logtemp

--- logtemp10.js
+++ logtemp10.js
@@ -1,9 +1,7 @@
 var parsetemp = require('./parsetemp');
 var setup = require('./setup');
-var fs = require('fs');
-var request = require('request');
-var dateformat = require('dateformat');
 var jf = require('jsonfile');
+var lt = require('./logtemp');
 
 w1_sensors = []
 for (let i=0; i<setup.sensors.length; ++i) {
@@ -18,87 +16,9 @@ var dht22 = parsetemp.getHTU21(12);
 var v = parsetemp.getw1temps(w1_sensors)
 console.log("t: " + v);
 
-try {
-	cput = Math.round(parseInt(fs.readFileSync('/sys/devices/virtual/thermal/thermal_zone0/temp')) / 1000.0);
-} catch (e) {
-	cput = "null";
-}
-
-function datapush(data, id, ts, value) {
-	data.push([dateformat(ts, "yyyy-mm-dd HH:MM"), id, value]);
-}
-
-function addpoint(ts, td, value, delta, data, lastts, id) {
-		if (td.lastvalues != value 
-			&& value > -50 // ignore impossible values!
-		) { 
-				var doit = true;
-
-				if (Math.abs(td.lastvalues - value) < delta) {
-						if (td.transition === null) {
-								td.transition = ts;
-								doit = false;
-						} else {
-								if (ts - td.transition > 17 * 60 * 1000) { // 17 minutes
-									  datapush(data, id, td.transition, td.lastvalues);
-										td.lasttss = lastts;
-										doit = true;
-								} else {
-										doit = false;
-								}
-						}
-				}
-				if (doit) {
-						if (lastts != td.lasttss && td.lasttss != 0) {
-							datapush(data, id, lastts, td.lastvalues);
-						}
-
-						datapush(data, id, ts,value);
-						td.lastvalues = value;
-						td.lasttss = ts;
-						td.transition = null;
-				}
-		} else {
-				td.transition = null;
-		}
-}
-
-
-function addtemp(data, ts, value, id, delta, transdata) {
-	if (value != "null" && value != null) {
-		if (!(id in transdata.data)) {
-			transdata.data[id] = {
-				lasttss: 0,
-				lastvalues: -100,
-				transition: null
-			};
-		}
-	  
-		addpoint(ts, transdata.data[id], value, delta, data, transdata.lastts, id);
-	}
-}
-
-const laterfile = '/tmp/sensors.later';
-const transfile = '/tmp/transition.json';
-const internet = '/tmp/temperature.json';
 parsetemp.getinternet((temp, humid, rain) => {
-
-	var transdata = {
-		lastts: 0,
-		data: {}
-	};
-	try {
-		transdata = JSON.parse(fs.readFileSync(transfile));
-	} catch (e) {}
-
-	var ts = Date.now();
-	var postdata = [];
-	try {
-		postdata = JSON.parse(fs.readFileSync(laterfile));
-		fs.unlinkSync(laterfile);
-	} catch(e) {		
-	}
 	
+	const internet = '/tmp/temperature.json';
 	jf.writeFileSync(internet, { 
 		out: {
 			humidity: humid, 
@@ -107,26 +27,20 @@ parsetemp.getinternet((temp, humid, rain) => {
 		in: dht22
 	});
 
-	addtemp(postdata, ts, humid, 100, 1.5, transdata);
-	addtemp(postdata, ts, dht22['humidity'], 101, 0.15, transdata);
-	addtemp(postdata, ts, temp, 0, 0.15, transdata);
-	addtemp(postdata, ts, dht22['temp'],1, 0.15, transdata);
-	addtemp(postdata, ts, cput, 2, 1.5, transdata);
-	addtemp(postdata, ts, rain, 400, 0.1, transdata);
+	lt.logtemp(function(cput, postdata, ts, transdata) {		
+		lt.addtemp(postdata, ts, humid, 100, 1.5, transdata);
+		lt.addtemp(postdata, ts, dht22['humidity'], 101, 0.15, transdata);
+		lt.addtemp(postdata, ts, temp, 0, 0.15, transdata);
+		lt.addtemp(postdata, ts, dht22['temp'],1, 0.15, transdata);
+		lt.addtemp(postdata, ts, cput, 2, 1.5, transdata);
+		lt.addtemp(postdata, ts, rain, 400, 0.1, transdata);
 
-	addtemp(postdata, ts, v[0], 3, 0.15, transdata);
-	addtemp(postdata, ts, v[1], 4, 0.15, transdata);
-	addtemp(postdata, ts, v[2], 5, 0.15, transdata);
-	addtemp(postdata, ts, v[3], 6, 0.15, transdata);
-	addtemp(postdata, ts, v[4], 7, 0.15, transdata);
+		lt.addtemp(postdata, ts, v[0], 3, 0.15, transdata);
+		lt.addtemp(postdata, ts, v[1], 4, 0.15, transdata);
+		lt.addtemp(postdata, ts, v[2], 5, 0.15, transdata);
+		lt.addtemp(postdata, ts, v[3], 6, 0.15, transdata);
+		lt.addtemp(postdata, ts, v[4], 7, 0.15, transdata);
+	});
 	
-	transdata.lastts = ts;
-	fs.writeFileSync(transfile, JSON.stringify(transdata));
-
 	require('./checkrelaisstate').check();
-	if (postdata.length > 0) {
-		console.log(postdata);
-		const postreq = require('./postrequest');
-		postreq.postrequest(laterfile, postdata, 0);
-	}
 });
