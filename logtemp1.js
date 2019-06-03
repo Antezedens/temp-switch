ad1988dda3e9089351ea86f3d6a79835f324306c
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2019-06-03 17:02:51 +0200

    generalize step 2

--- logtemp1.js
+++ logtemp1.js
@@ -1,89 +1,11 @@
 var parsetemp = require('./parsetemp');
 var setup = require('./setup');
 var fs = require('fs');
-var request = require('request');
-var dateformat = require('dateformat');
+var lt = require('./logtemp');
 
 var dht22 = parsetemp.getDHT22(6);
 
-try {
-	cput = Math.round(parseInt(fs.readFileSync('/sys/devices/virtual/thermal/thermal_zone0/temp')) / 1000.0);
-} catch (e) {
-	cput = "null";
-}
-function datapush(data, id, ts, value) {
-	data.push([dateformat(ts, "yyyy-mm-dd HH:MM"), id, value]);
-}
-
-function addpoint(ts, td, value, delta, data, lastts, id) {
-		if (td.lastvalues != value 
-			&& value > -50 // ignore impossible values!
-		) { 
-				var doit = true;
-
-				if (Math.abs(td.lastvalues - value) < delta) {
-						if (td.transition === null) {
-								td.transition = ts;
-								doit = false;
-						} else {
-								if (ts - td.transition > 17 * 60 * 1000) { // 17 minutes
-									  datapush(data, id, td.transition, td.lastvalues);
-										td.lasttss = lastts;
-										doit = true;
-								} else {
-										doit = false;
-								}
-						}
-				}
-				if (doit) {
-						if (lastts != td.lasttss && td.lasttss != 0) {
-							datapush(data, id, lastts, td.lastvalues);
-						}
-
-						datapush(data, id, ts,value);
-						td.lastvalues = value;
-						td.lasttss = ts;
-						td.transition = null;
-				}
-		} else {
-				td.transition = null;
-		}
-}
-
-
-function addtemp(data, ts, value, id, delta, transdata) {
-	if (value != "null" && value != null) {
-		if (!(id in transdata.data)) {
-			transdata.data[id] = {
-				lasttss: 0,
-				lastvalues: 0,
-				transition: null
-			};
-		}
-	  
-		addpoint(ts, transdata.data[id], value, delta, data, transdata.lastts, id);
-	}
-}
-
-const laterfile = '/tmp/sensors.later';
-const transfile = '/tmp/transition.json';
 const statsfile = '/tmp/netstats.json';
-
-var transdata = {
-	lastts: 0,
-	data: {}
-};
-try {
-	transdata = JSON.parse(fs.readFileSync(transfile));
-} catch (e) {}
-
-var ts = Date.now();
-var postdata = [];
-try {
-	postdata = JSON.parse(fs.readFileSync(laterfile));
-	fs.unlinkSync(laterfile);
-} catch(e) {		
-}
 
 var oldstats = [];
 try {
@@ -91,6 +13,7 @@ try {
 } catch(e) {
 }
 
+var netstats = {};
 try {
 	//let iface = "enp3s0";enx0c5b8f279a64
 	let iface = "enx0c5b8f279a64";
@@ -100,31 +23,22 @@ try {
 	};
 	fs.writeFileSync(statsfile, JSON.stringify(netstats));
 } catch(e) {
-	netstats = "null";
 }
 
 function convertToMb(value) {
 	return Math.round(value / (1024.0 * 10.24)) / 100.0;
 }
-if ('up' in netstats && 'up' in oldstats) {
-	let upRate = convertToMb(netstats.up - oldstats.up);
-	let downRate = convertToMb(netstats.down - oldstats.down);
-	addtemp(postdata, ts, upRate, 312, 0.1, transdata);
-	addtemp(postdata, ts, downRate, 313, 0.1, transdata);
-}
 
-addtemp(postdata, ts, dht22['humidity'], 108, 0.15, transdata);
-addtemp(postdata, ts, dht22['temp'],8, 0.15, transdata);
-addtemp(postdata, ts, cput, 9, 10, transdata);
+lt.logtemp(function(cput, postdata, ts, transdata) {	
+	if ('up' in netstats && 'up' in oldstats) {
+		let upRate = convertToMb(netstats.up - oldstats.up);
+		let downRate = convertToMb(netstats.down - oldstats.down);
+		addtemp(postdata, ts, upRate, 312, 0.1, transdata);
+		addtemp(postdata, ts, downRate, 313, 0.1, transdata);
+	}
+
+	lt.addtemp(postdata, ts, dht22['humidity'], 108, 0.15, transdata);
+	lt.addtemp(postdata, ts, dht22['temp'],8, 0.15, transdata);
+	lt.addtemp(postdata, ts, cput, 9, 10, transdata);
 	
-transdata.lastts = ts;
-fs.writeFileSync(transfile, JSON.stringify(transdata));
-
-if (postdata.length > 0) {
-	console.log(postdata);
-	const postreq = require('./postrequest');
-	postreq.postrequest(laterfile, postdata, 0);
-}
-
-//require('./checkrelaisstate').check();
-// /sys/devices/virtual/thermal/thermal_zone0/temp
+});
