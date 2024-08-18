793dfaaba69ae3a3365db04f5f3274a058b221db
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2024-08-18 09:21:20 +0200

    poll: convert to gpiod-2.2.x

--- poll.py
+++ poll.py
@@ -1,47 +1,34 @@
 #!/usr/bin/env python3
 
-import sys
-from datetime import timedelta, datetime
-from gpiod import chip, line_request, line_event
+from datetime import timedelta
+import gpiod
+from gpiod.line import Direction, Value, Edge, Bias
 import requests
-import time
 import subprocess
 
 CHIP = 0
 LINE_OFFSET = 1 # 6
-#LED_LINE_OFFSET = 198
-EDGE = line_request.EVENT_BOTH_EDGES
 
-c = chip(CHIP)
-button = c.get_line(LINE_OFFSET)
 #led = c.get_line(LED_LINE_OFFSET)
 
-config = line_request()
-config.consumer = "Button"
-config.request_type = EDGE
-config.flags = line_request.FLAG_BIAS_PULL_UP
-
-#ledreq = line_request()
-#ledreq.consumer = "LED"
-#ledreq.request_type = line_request.DIRECTION_OUTPUT
-
-button.request(config)
-#led.request(ledreq)
-
-#while True:
-#    led.set_value(0)
-#    time.sleep(0.3)
-#    led.set_value(1)
-#    time.sleep(0.3)
-
-#requests.get('http://localhost:5000/toggle_irrigation?pin=198,199&times=60m')
+button = gpiod.request_lines(
+    "/dev/gpiochip%d" % CHIP,
+    consumer="poll.py",
+    config = {
+        LINE_OFFSET: gpiod.LineSettings(
+            direction=Direction.INPUT,
+            edge_detection = Edge.BOTH,
+            bias = Bias.PULL_UP,
+        )
+    },
+)
 
 while True:
-    if button.event_wait(timedelta(seconds=600)):
+    if button.wait_edge_events(timedelta(seconds=600)):
         # event_read() is blocking function.
-        event = button.event_read()
-        if button.get_value() == 0:
-            if button.event_wait(timedelta(milliseconds=400)):
+        event = button.read_edge_events()
+        if button.get_value(LINE_OFFSET) == Value.INACTIVE:
+            if button.wait_edge_events(timedelta(milliseconds=400)):
                 subprocess.run(["logger", "button pressed not long enough"])
             else:
                 subprocess.run(["logger", "button pressed for 0.4s"])
