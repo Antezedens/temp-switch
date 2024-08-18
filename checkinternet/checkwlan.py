edceb10a7d900cab90e5e4b66797e46bfbc673da
Bernhard Fuchs <bernhard.fuchs@riedel.net>
2024-08-18 09:37:26 +0200

    fix checkwlan.py

--- checkinternet/checkwlan.py
+++ checkinternet/checkwlan.py
@@ -4,7 +4,7 @@ import time
 
 for i in range(100):
     start = time.time()
-    if subprocess.run(["ping", "-c1", "10.5.5.1"]).returncode == 0:
+    if subprocess.run(["ping", "-c1", "10.5.5.20"]).returncode == 0:
     #if subprocess.run(["ping", "-c1", "40.40.40.40"]).returncode == 0:
     #if subprocess.run(["ping", "-c1", "1www.google.at"]).returncode == 0:
         subprocess.run(["logger", "wlan check ok"])
@@ -12,7 +12,10 @@ for i in range(100):
     elapsed = time.time() - start
     if elapsed < 10:
         time.sleep(10 - elapsed)
+    if (i % 10) == 9:
+        subprocess.run(["logger", "reconnecting wlan"])
+        subprocess.run(["ifconfig", "wlx40a5efd39dc7", "down"])
+        subprocess.run(["ifconfig", "wlx40a5efd39dc7", "up"])
 
-    
 subprocess.run(["logger", "wlan down rebooting"])
 subprocess.run(["reboot"])
